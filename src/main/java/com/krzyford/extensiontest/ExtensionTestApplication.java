aca38be46e8a545781229877dd846de0becbb334
Tom <thomas.l.burford@gmail.com>
2023-02-09 19:14:57 -0500

    First Commit

---
+++ src/main/java/com/krzyford/extensiontest/ExtensionTestApplication.java
@@ -0,0 +1,13 @@
+package com.krzyford.extensiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krzyford/extensiontest/ExtensionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzyford.extensiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
